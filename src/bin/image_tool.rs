73da96d6eea7f3f430480abe68108915b7c9b7a3
Aditya Mandal <mandaladitya1614@gmail.com>
2025-08-09 15:24:06 +0530

    Implemented saturation

--- src/bin/image_tool.rs
+++ src/bin/image_tool.rs
@@ -37,7 +37,7 @@ fn main() -> Result<()> {
     let output_path = &args[3];
     let mut config = AsciiConfig::default();
     
-    for arg in &args[3..] {
+    for arg in &args[4..] {
         match arg.as_str() {
             "--invert" => config.invert = true,
             "--detailed" => config.detailed = true,
@@ -107,6 +107,18 @@ fn main() -> Result<()> {
             println!("Applying sharpen filter with strength: {}", strength);
             let sharpened_img = sharpen(&img, strength);
             sharpened_img.save(output_path)?;
+        }
+        "saturate" => {
+            if args.len() != 5 {
+                eprintln!("Error: saturate mode requires a factor");
+                return Ok(());
+            }
+            let factor: f32 = args[4].parse()?;
+            // let saturation = img.adjust_saturation(factor);
+            let saturated_img = adjust_saturation(&img, factor);
+
+            saturated_img.save(output_path)?;
+        
         }
         "rotate90" => {
             println!("Rotating image 90 degree clock wise...");
@@ -436,6 +448,79 @@ fn adjust_contrast(img: &DynamicImage, factor: f32) -> ImageBuffer<Rgb<u8>, Vec<
             let new_green = (factor * (g as f32 - 128.0) + 128.0).clamp(0.0, 255.0) as u8;
             let new_blue = (factor * (b as f32 - 128.0) + 128.0).clamp(0.0, 255.0) as u8;
 
+            output.put_pixel(x, y, Rgb([new_red, new_green, new_blue]));
+        }
+    }
+    output
+}
+
+fn rgb_to_hsl(r:u8, g:u8, b:u8) -> (f32, f32, f32) {
+    let red_normal = r as f32 / 255.0;
+    let green_normal = g as f32 / 255.0;
+    let blue_normal = b as f32 / 255.0;
+
+
+    let max = red_normal.max(green_normal.max(blue_normal));
+    let min = red_normal.min(green_normal.min(blue_normal));
+
+    let lightness = (max + min) / 2.0;
+    let mut hue = 0.0;
+    let mut saturation = 0.0;
+
+    if max != min {
+        let diff = max - min;
+        saturation = if lightness > 0.5 {diff / (2.0 - max - min)} else {diff / (max + min)};
+        hue = match max {
+            x if x == red_normal => (green_normal - blue_normal) / diff + (if green_normal < blue_normal {6.0} else {0.0}),
+            x if x == green_normal => (blue_normal - red_normal) / diff + 2.0,
+            _=> (red_normal - green_normal) / diff + 4.0,
+        };
+        hue /= 6.0;
+    }
+    (hue * 360.0, saturation, lightness)
+}
+
+fn hsl_to_rgb(h:f32, s: f32, l: f32) -> (u8, u8, u8) {
+    if s == 0.0 {
+        let val = (l * 255.0) as u8;
+        return (val, val, val);
+    }
+
+    let hue_to_rgb = |p: f32, q: f32, mut t: f32| {
+        if t < 0.0 { t += 1.0;}
+        if t > 1.0 { t -= 1.0;}
+        if t < 1.0 / 6.0 { return p + (q-p) * 6.0 * t;  }
+        if t < 1.0 / 2.0 { return q; }
+        if t < 2.0 / 3.0 { return p + (q - p) * (2.0 / 3.0 - t) * 6.0;}
+        p
+    };
+
+    let q = if l < 0.5 { l * (1.0 + s)} else { l + s - l * s};
+    let p = 2.0 * l - q;
+    let hue_normal = h / 360.0;
+
+
+    let red = hue_to_rgb(p, q, hue_normal + 1.0 / 3.0);
+    let green = hue_to_rgb(p, q, hue_normal);
+    let blue = hue_to_rgb(p, q, hue_normal - 1.0 / 3.0);
+
+    ((red * 255.0) as u8, (green * 255.0) as u8, (blue * 255.0) as u8)
+
+}
+
+fn adjust_saturation(img: &DynamicImage, factor: f32) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    let (width, height) = img.dimensions();
+    let mut output = ImageBuffer::<Rgb<u8>, _>::new(width, height);
+
+    for y in 0..height {
+        for x in 0..width {
+
+            let Rgba([r, g, b, _]) = img.get_pixel(x, y);
+            let (h, s, l) = rgb_to_hsl(r, g, b);
+            let new_s = (s * factor).clamp(0.0, 1.0);
+            let (new_red, new_green, new_blue) = hsl_to_rgb(h, new_s, l);
+
+            output.put_pixel(x, y, Rgb([new_red, new_green, new_blue]));
             output.put_pixel(x, y, Rgb([new_red, new_green, new_blue]));
         }
     }
