c606cd2e4f4987e6eb8340573d11c33cd6a0895b
Aditya Mandal <mandaladitya1614@gmail.com>
2025-08-16 23:00:20 +0530

    Optimized oil_painting (same as box blur)

--- src/artistic.rs
+++ src/artistic.rs
@@ -1,5 +1,5 @@
-use image::{DynamicImage, GenericImageView, ImageBuffer, Rgb};
-use rayon::{iter::{IndexedParallelIterator, ParallelIterator}, slice::{ParallelSlice, ParallelSliceMut}};
+use image::{DynamicImage, ImageBuffer, Rgb};
+use rayon::{iter::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator}, slice::{ParallelSlice, ParallelSliceMut}};
 use crate::{ProgressSender, send_progress};
 
 
@@ -29,26 +29,6 @@ pub fn sepia(img: &DynamicImage, progress_tx: Option<ProgressSender>) -> ImageBu
             out_pixel[1] = new_green.min(255.0) as u8;
             out_pixel[2] = new_blue.min(255.0) as u8;
         });
-
-    // for y in 0..height {
-    //     for x in 0..width {
-    //         let Rgba([r, g, b, _]) = img.get_pixel(x, y);
-        
-    //         let red_filter = r as f32;
-    //         let green_filter = g as f32;
-    //         let blue_filter = b as f32;
-
-    //         let new_red = (red_filter * 0.393) + (green_filter * 0.769) + (blue_filter * 0.189);
-    //         let new_green = (red_filter * 0.349) + (green_filter * 0.686) + (blue_filter * 0.168);
-    //         let new_blue = (red_filter * 0.272) + (green_filter * 0.534) + (blue_filter * 0.131);
-
-    //         output.put_pixel(x, y, Rgb([
-    //             new_red.min(255.0) as u8,
-    //             new_green.min(255.0) as u8,
-    //             new_blue.min(255.0) as u8,
-    //         ]));
-    //     }
-    // }
 
     send_progress(&progress_tx, 1.0);
     out_buffer
@@ -88,21 +68,6 @@ pub fn vignette(img: &DynamicImage, strength: f32, progress_tx: Option<ProgressS
                 out_row[out_idx + 2] = (in_pixels[in_idx + 2] as f32 * vignette_factor) as u8;
             }
         });
-
-    // for y in 0..height {
-    //     for x in 0..width {
-    //         let Rgba([r, g, b, _]) = img.get_pixel(x, y);
-            
-    //         let distance = ((x as f32 - center_x).powi(2) + (y as f32 - center_y).powi(2)).sqrt();
-    //         let vignette_factor = 1.0 - (distance / max_distance * strength).clamp(0.0, 1.0);
-            
-    //         let new_red = (r as f32 * vignette_factor) as u8;
-    //         let new_green = (g as f32 * vignette_factor) as u8;
-    //         let new_blue = (b as f32 * vignette_factor) as u8;
-            
-    //         output.put_pixel(x, y, Rgb([new_red, new_green, new_blue]));
-    //     }
-    // }
     
     send_progress(&progress_tx, 1.0);
     out_buffer
@@ -114,7 +79,6 @@ pub fn noise(img: &DynamicImage, strength: u8, progress_tx: Option<ProgressSende
     let rgb_img = img.to_rgb8();
     let (width, height) = rgb_img.dimensions();
     let mut out_buffer = ImageBuffer::new(width, height);
-    // let mut rng = rand::rng();
     
     send_progress(&progress_tx, 0.0);
 
@@ -142,147 +106,228 @@ pub fn noise(img: &DynamicImage, strength: u8, progress_tx: Option<ProgressSende
                 out_row[out_idx + 2] = (in_pixels[in_idx + 2] as i32 + noise_blue).clamp(0, 255) as u8;
             }
         });
-
-    // for y in 0..height {
-    //     for x in 0..width {
-    //         let Rgba([r, g, b, _]) = img.get_pixel(x, y);
-            
-    //         let noise_r = rng.random_range(-(strength as i32)..=strength as i32);
-    //         let noise_g = rng.random_range(-(strength as i32)..=strength as i32);
-    //         let noise_b = rng.random_range(-(strength as i32)..=strength as i32);
-            
-    //         let new_red = (r as i32 + noise_r).clamp(0, 255) as u8;
-    //         let new_green = (g as i32 + noise_g).clamp(0, 255) as u8;
-    //         let new_blue = (b as i32 + noise_b).clamp(0, 255) as u8;
-            
-    //         output.put_pixel(x, y, Rgb([new_red, new_green, new_blue]));
-    //     }
-    // }
     
     send_progress(&progress_tx, 1.0);
     out_buffer
 }
 
-/// Apply oil painting effect
 pub fn oil_painting(img: &DynamicImage, radius: u32, intensity: u32, progress_tx: Option<ProgressSender>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
-
     let rgb_img = img.to_rgb8();
-    let (width, height) = img.dimensions();
-    let mut out_buffer = ImageBuffer::<Rgb<u8>, _>::new(width, height);
+    let (width, height) = rgb_img.dimensions();
     
     send_progress(&progress_tx, 0.0);
 
-    let in_pixels = rgb_img.as_raw();
-
-    out_buffer.as_mut()
-        .par_chunks_exact_mut((width * 3) as usize)
-        .enumerate()
-        .for_each(|(y, out_row)| {
-            for x in 0..width {
-                let mut intensity_count = vec![0; intensity as usize];
-                let mut avg_red = vec![0.0; intensity as usize];
-                let mut avg_green = vec![0.0; intensity as usize];
-                let mut avg_blue = vec![0.0; intensity as usize];
-
-                let y_min = (y as u32).saturating_sub(radius);
-                let y_max = ((y as u32) + radius).min(height - 1);
-                let x_min = x.saturating_sub(radius);
-                let x_max = (x + radius).min(width - 1);
-
-                for new_y in y_min..=y_max {
-                    for new_x in x_min..=x_max {
-                        let sample_idx = ((new_y * width + new_x) * 3) as usize;
-                        let red = in_pixels[sample_idx];
-                        let green = in_pixels[sample_idx + 1];
-                        let blue = in_pixels[sample_idx + 2];
-
-                        let intensity_index = ((red as u32 + green as u32 + blue as u32) * intensity / (3 * 256))
-                            .min(intensity - 1)as usize;
-                        
-                        intensity_count[intensity_index] += 1;
-                        avg_red[intensity_index] += red as f32;
-                        avg_green[intensity_index] += green as f32;
-                        avg_blue[intensity_index] += blue as f32;
-                    } 
-                }
-
-                let max_index = intensity_count
-                    .iter()
-                    .enumerate()
-                    .max_by_key(|(_, count)| *count)
-                    .map(|(idx, _)| idx)
-                    .unwrap_or(0);
-
-
-                let out_idx = (x * 3) as usize;
-                if intensity_count[max_index] > 0 {
-                    let count = intensity_count[max_index] as f32;
-                    out_row[out_idx] = (avg_red[max_index] / count) as u8;
-                    out_row[out_idx + 1] = (avg_green[max_index] / count) as u8;
-                    out_row[out_idx + 2] = (avg_blue[max_index] / count) as u8;
-                } else {
-                    out_row[out_idx] = 0;
-                    out_row[out_idx + 1] = 0;
-                    out_row[out_idx + 2] = 0;
-                }
-            }
-        });
-
-    // for y in 0..height {
-    //     for x in 0..width {
-    //         let mut intensity_count = vec![0; intensity as usize];
-    //         let mut avg_red = vec![0.0; intensity as usize];
-    //         let mut avg_green = vec![0.0; intensity as usize];
-    //         let mut avg_blue = vec![0.0; intensity as usize];
-
-    //         let y_min = y.saturating_sub(radius);
-    //         let y_max = (y + radius).min(height - 1);
-    //         let x_min = x.saturating_sub(radius);
-    //         let x_max = (x + radius).min(width - 1);
-
-    //         for ny in y_min..=y_max {
-    //             for nx in x_min..=x_max {
-    //                 let Rgba([r, g, b, _]) = img.get_pixel(nx, ny);
-    //                 let intensity_index = ((r as u32 + g as u32 + b as u32) * intensity / (3 * 256)).min(intensity - 1) as usize;
-
-    //                 intensity_count[intensity_index] += 1;
-    //                 avg_red[intensity_index] += r as f32;
-    //                 avg_green[intensity_index] += g as f32;
-    //                 avg_blue[intensity_index] += b as f32;
-    //             }
-    //         }
-
-    //         let mut max_index = 0;
-    //         for i in 1..intensity as usize {
-    //             if intensity_count[i] > intensity_count[max_index] {
-    //                 max_index = i;
-    //             }
-    //         }
-
-    //         let final_red = if intensity_count[max_index] > 0 {
-    //             (avg_red[max_index] / intensity_count[max_index] as f32) as u8
-    //         } else {
-    //             0
-    //         };
-    //         let final_green = if intensity_count[max_index] > 0 {
-    //             (avg_green[max_index] / intensity_count[max_index] as f32) as u8
-    //         } else {
-    //             0
-    //         };
-    //         let final_blue = if intensity_count[max_index] > 0 {
-    //             (avg_blue[max_index] / intensity_count[max_index] as f32) as u8
-    //         } else {
-    //             0
-    //         };
-
-    //         out_buffer.put_pixel(x, y, Rgb([final_red, final_green, final_blue]));
-    //     }
-        
-    //     if y % 5 == 0 {
-    //         send_progress(&progress_tx, y as f64 / height as f64);
-    //     }
-    // }
+    let horizontal_result = horizontal_oil_pass(&rgb_img, radius, intensity, width, height);
+    
+    send_progress(&progress_tx, 0.5);
+    
+    let final_result = vertical_oil_pass(&horizontal_result, radius, intensity, width, height);
     
     send_progress(&progress_tx, 1.0);
-    out_buffer
+    
+    ImageBuffer::from_vec(width, height, final_result).unwrap()
+}
+
+
+fn horizontal_oil_pass(img: &ImageBuffer<Rgb<u8>, Vec<u8>>, radius: u32, intensity: u32, width: u32, height: u32) -> Vec<u8> {
+    let mut result = vec![0u8; (width * height * 3) as usize];
+    let pixels = img.as_raw();
+    
+    result.par_chunks_exact_mut((width * 3) as usize).enumerate().for_each(|(y, row)| {
+        let row_start = (y as u32 * width * 3) as usize;
+        
+        let mut intensity_count = vec![0u32; intensity as usize];
+        let mut avg_red = vec![0.0f32; intensity as usize];
+        let mut avg_green = vec![0.0f32; intensity as usize];
+        let mut avg_blue = vec![0.0f32; intensity as usize];
+        
+        let x_min = 0u32.saturating_sub(radius);
+        let x_max = (0 + radius).min(width - 1);
+        
+        for kx in x_min..=x_max {
+            let idx = row_start + (kx * 3) as usize;
+            let red = pixels[idx];
+            let green = pixels[idx + 1];
+            let blue = pixels[idx + 2];
+            
+            let intensity_idx = ((red as u32 + green as u32 + blue as u32) * intensity / (3 * 256))
+                .min(intensity - 1) as usize;
+            
+            intensity_count[intensity_idx] += 1;
+            avg_red[intensity_idx] += red as f32;
+            avg_green[intensity_idx] += green as f32;
+            avg_blue[intensity_idx] += blue as f32;
+        }
+        
+        let max_idx = find_dominant_intensity(&intensity_count);
+        set_oil_pixel(row, 0, max_idx, &intensity_count, &avg_red, &avg_green, &avg_blue);
+        
+        for x in 1..width {
+            let new_x_min = x.saturating_sub(radius);
+            let new_x_max = (x + radius).min(width - 1);
+            let prev_x_min = (x - 1).saturating_sub(radius);
+            let prev_x_max = ((x - 1) + radius).min(width - 1);
+            
+            if new_x_min > prev_x_min {
+                let remove_idx = row_start + (prev_x_min * 3) as usize;
+                let red = pixels[remove_idx];
+                let green = pixels[remove_idx + 1]; 
+                let blue = pixels[remove_idx + 2];
+                
+                let intensity_idx = ((red as u32 + green as u32 + blue as u32) * intensity / (3 * 256))
+                    .min(intensity - 1) as usize;
+                
+                intensity_count[intensity_idx] -= 1;
+                avg_red[intensity_idx] -= red as f32;
+                avg_green[intensity_idx] -= green as f32;
+                avg_blue[intensity_idx] -= blue as f32;
+            }
+            
+            if new_x_max > prev_x_max {
+                let add_idx = row_start + (new_x_max * 3) as usize;
+                let red = pixels[add_idx];
+                let green = pixels[add_idx + 1];
+                let blue = pixels[add_idx + 2];
+                
+                let intensity_idx = ((red as u32 + green as u32 + blue as u32) * intensity / (3 * 256))
+                    .min(intensity - 1) as usize;
+                
+                intensity_count[intensity_idx] += 1;
+                avg_red[intensity_idx] += red as f32;
+                avg_green[intensity_idx] += green as f32;
+                avg_blue[intensity_idx] += blue as f32;
+            }
+            
+            let max_idx = find_dominant_intensity(&intensity_count);
+            set_oil_pixel(row, x, max_idx, &intensity_count, &avg_red, &avg_green, &avg_blue);
+        }
+    });
+    
+    result
+}
+
+fn vertical_oil_pass(pixels: &[u8], radius: u32, intensity: u32, width: u32, height: u32) -> Vec<u8> {
+    let result = vec![0u8; (width * height * 3) as usize];
+    
+    (0..width).into_par_iter().for_each(|x| {
+
+        let mut intensity_count = vec![0u32; intensity as usize];
+        let mut avg_red = vec![0.0f32; intensity as usize];
+        let mut avg_green = vec![0.0f32; intensity as usize];
+        let mut avg_blue = vec![0.0f32; intensity as usize];
+        
+        let y_min = 0u32.saturating_sub(radius);
+        let y_max = (0 + radius).min(height - 1);
+        
+        for ky in y_min..=y_max {
+            let idx = ((ky * width + x) * 3) as usize;
+            let red = pixels[idx];
+            let green = pixels[idx + 1];
+            let blue = pixels[idx + 2];
+            
+            let intensity_idx = ((red as u32 + green as u32 + blue as u32) * intensity / (3 * 256))
+                .min(intensity - 1) as usize;
+            
+            intensity_count[intensity_idx] += 1;
+            avg_red[intensity_idx] += red as f32;
+            avg_green[intensity_idx] += green as f32;
+            avg_blue[intensity_idx] += blue as f32;
+        }
+        
+        let out_idx = (x * 3) as usize;
+        let max_idx = find_dominant_intensity(&intensity_count);
+        unsafe {
+            let result_ptr = result.as_ptr() as *mut u8;
+            set_oil_pixel_unsafe(result_ptr, out_idx, max_idx, &intensity_count, &avg_red, &avg_green, &avg_blue);
+        }
+        
+        for y in 1..height {
+            let new_y_min = y.saturating_sub(radius);
+            let new_y_max = (y + radius).min(height - 1);
+            let prev_y_min = (y - 1).saturating_sub(radius);
+            let prev_y_max = ((y - 1) + radius).min(height - 1);
+            
+            if new_y_min > prev_y_min {
+                let remove_idx = ((prev_y_min * width + x) * 3) as usize;
+                let red = pixels[remove_idx];
+                let green = pixels[remove_idx + 1];
+                let blue = pixels[remove_idx + 2];
+                
+                let intensity_idx = ((red as u32 + green as u32 + blue as u32) * intensity / (3 * 256))
+                    .min(intensity - 1) as usize;
+                
+                intensity_count[intensity_idx] -= 1;
+                avg_red[intensity_idx] -= red as f32;
+                avg_green[intensity_idx] -= green as f32;
+                avg_blue[intensity_idx] -= blue as f32;
+            }
+            
+            if new_y_max > prev_y_max {
+                let add_idx = ((new_y_max * width + x) * 3) as usize;
+                let red = pixels[add_idx];
+                let green = pixels[add_idx + 1];
+                let blue = pixels[add_idx + 2];
+                
+                let intensity_idx = ((red as u32 + green as u32 + blue as u32) * intensity / (3 * 256))
+                    .min(intensity - 1) as usize;
+                
+                intensity_count[intensity_idx] += 1;
+                avg_red[intensity_idx] += red as f32;
+                avg_green[intensity_idx] += green as f32;
+                avg_blue[intensity_idx] += blue as f32;
+            }
+            
+            let out_idx = ((y * width + x) * 3) as usize;
+            let max_idx = find_dominant_intensity(&intensity_count);
+            unsafe {
+                let result_ptr = result.as_ptr() as *mut u8;
+                set_oil_pixel_unsafe(result_ptr, out_idx, max_idx, &intensity_count, &avg_red, &avg_green, &avg_blue);
+            }
+        }
+    });
+    
+    result
+}
+
+#[inline(always)]
+fn find_dominant_intensity(intensity_count: &[u32]) -> usize {
+    intensity_count
+        .iter()
+        .enumerate()
+        .max_by_key(|(_, count)| *count)
+        .map(|(idx, _)| idx)
+        .unwrap_or(0)
+}
+
+#[inline(always)]
+fn set_oil_pixel(row: &mut [u8], x: u32, max_idx: usize, intensity_count: &[u32], avg_red: &[f32], avg_green: &[f32], avg_blue: &[f32]) {
+    let out_idx = (x * 3) as usize;
+    if intensity_count[max_idx] > 0 {
+        let count = intensity_count[max_idx] as f32;
+        row[out_idx] = (avg_red[max_idx] / count) as u8;
+        row[out_idx + 1] = (avg_green[max_idx] / count) as u8;
+        row[out_idx + 2] = (avg_blue[max_idx] / count) as u8;
+    } else {
+        row[out_idx] = 0;
+        row[out_idx + 1] = 0;
+        row[out_idx + 2] = 0;
+    }
+}
+
+#[inline(always)]
+unsafe fn set_oil_pixel_unsafe(result_ptr: *mut u8, out_idx: usize, max_idx: usize, intensity_count: &[u32], avg_red: &[f32], avg_green: &[f32], avg_blue: &[f32]) {
+    if intensity_count[max_idx] > 0 {
+        let count = intensity_count[max_idx] as f32;
+        unsafe {
+            *result_ptr.add(out_idx) = (avg_red[max_idx] / count) as u8;
+            *result_ptr.add(out_idx + 1) = (avg_green[max_idx] / count) as u8;
+            *result_ptr.add(out_idx + 2) = (avg_blue[max_idx] / count) as u8;
+        }
+    } else {
+        unsafe {
+            *result_ptr.add(out_idx) = 0;
+            *result_ptr.add(out_idx + 1) = 0;
+            *result_ptr.add(out_idx + 2) = 0;
+        }
+    }
 }
---
+++ test_artistic.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "🎨 MOGGU ARTISTIC EFFECTS BENCHMARK"
+echo "==================================="
+
+IMAGE="test_images/Arcade_decay_red.png"
+
+echo "📸 Input: $IMAGE"
+echo ""
+
+echo "🌟 Testing sepia effect..."
+hyperfine --warmup 1 "./target/release/benchmark '$IMAGE' sepia"
+
+echo "🖤 Testing vignette effect..."
+hyperfine --warmup 1 "./target/release/benchmark '$IMAGE' vignette 0.8"
+
+echo "📡 Testing noise effect..."
+hyperfine --warmup 1 "./target/release/benchmark '$IMAGE' noise 25"
+
+echo "🎭 Testing oil painting effect..."
+hyperfine --warmup 1 "./target/release/benchmark '$IMAGE' oil_painting 3 8"
+
